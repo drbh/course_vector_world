9e88bd1d919e8d117f2b7d5c051e467858584a09
David Holtz <david.richard.holtz@gmail.com>
2019-10-21 14:40:20 -0400

    add snapshot

--- 4. Extracting Useful Values/dask-grpby.py
+++ 4. Extracting Useful Values/dask-grpby.py
@@ -8,11 +8,14 @@ start = datetime.datetime.now()
 df = dd.read_csv("/home/drbh/Desktop/complete_list_of_similar_classes.csv")
 df = df.persist()
 
-g = df.groupby("Y").apply(lambda x: x.sort_values(by="S",ascending=False).head(2), meta={"X":"f8","Y":"f8","S":"f8"})
+g = df.groupby("Y").apply(lambda x: x.sort_values(by="S",ascending=False).head(6), meta={"X":"f8","Y":"f8","S":"f8"})
 ooutput = g.compute()
 
 end = datetime.datetime.now()
 
-ooutput.to_csv("topn.csv")
+ooutput.columns = ["target","source","value"]
+ooutput = ooutput.reset_index(drop=True)
+ooutput = ooutput[["source","target","value"]]
+ooutput.to_csv("topn.csv", index=False)
 
 print(end-start)
